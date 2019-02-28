50776b359ddc0d554643fea9c5c303c6cd981f60
Christian Soler <solero93@gmail.com>
2019-02-28 18:27:15 +0100

    Updated parser to be more generic

--- parser.py
+++ parser.py
@@ -1,10 +1,12 @@
-with open('input_file', 'r') as f:
-    R, C, L, H = map(lambda x: int(x), f.readline().split())
-    matrix = [[char for char in line.strip()] for line in f]
+def parse_input(filename):
+    with open(filename, 'r') as f:
+        R, C, L, H = map(lambda x: int(x), f.readline().split())
+        matrix = [[char for char in line.strip()] for line in f]
+    return R, C, L, H, matrix
 
 
-def algorithm():
-    pass
+def algorithm(*args):
+    return 0
 
 
 def write_result(result):
@@ -15,7 +17,8 @@ def write_result(result):
             result_file.write(' '.join(str(elem) for elem in line) + '\n')
 
 
-result = algorithm()
+R, C, L, H, matrix = parse_input('filename')
+result = algorithm(R, C, L, H, matrix)
 write_result(result)
 
 print('')
